53931856d0dc27ee5b041138cc81557cfaa6c097
indra <ispandey81@gmail.com>
2021-08-04 23:20:58 +0800

    Fixed quit flow

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -4,46 +4,61 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Cli {
-    private static final String searchOptions = "Welcome to Zendesk Search\nType 'quit to exit at any time, Press 'Enter' to continue\n\n\t\tSelect search options:\n\t\t* Press 1 to search Zendesk\n\t\t* Press 2 to view a list of searchable fields\n\t\t* Type 'quit' to exit\n\n";
+    private static final String searchOptions = "Welcome to Zendesk Search\nType 'quit' to exit at any time, Press 'Enter' to continue\n\n\t\tSelect search options:\n\t\t* Press 1 to search Zendesk\n\t\t* Press 2 to view a list of searchable fields\n\t\t* Type 'quit' to exit\n\n";
     private static final String usersFileName = "data/users.json";
     private static final String ticketsFileName = "data/tickets.json";
+    private static final String terminateProgramKeyword = "quit";
 
     public static void main(String[] args) {
-        Util.printHelp(searchOptions);
         try {
-            boolean quit = false;
-            while (true) {
-                Scanner scanner = new Scanner(System.in);
-                String userInput = scanner.nextLine();
-                if (userInput.equals("quit")) {
-                    System.out.println("Exiting the program");
-                    System.exit(0);
-                } else if (userInput.equals("1")) {
-                    System.out.println("Select 1) Users or 2) Tickets");
-                    String searchUsersOrTickets= scanner.nextLine();
-                    if (searchUsersOrTickets.equals("1")) {
-                        System.out.println("Enter search term");
-                        String searchTerm = scanner.nextLine();
-                        System.out.println("Enter search value");
-                        String searchValue = scanner.nextLine();
-                        System.out.printf("Searching users for %s with a value of %s \n", searchTerm, searchValue);
-                        List<User> userList = Util.findUser(searchTerm, searchValue, usersFileName);
-                        if (userList.isEmpty()) {
-                            System.out.println("No results found");
-                        } else {
-                            userList.stream().forEach(user -> System.out.format("%s", user.toString()));
+            Scanner scanner = new Scanner(System.in);
+            Util.printHelp(searchOptions);
+            String userInput = scanner.nextLine();
+            while (!userInput.equals(terminateProgramKeyword)) {
+                userInput = scanner.nextLine();
+                if (userInput.equals("1") || userInput.equals("2") || userInput.equals("")) {
+                    if (userInput.equals("1") && !userInput.equals(terminateProgramKeyword)) {
+                        System.out.println("Select 1) Users or 2) Tickets");
+                        userInput = scanner.nextLine();
+                        if (userInput.equals("1") && !userInput.equals(terminateProgramKeyword)) {
+                            // We are in user search
+                            System.out.println("Enter search term");
+                            userInput = scanner.nextLine();
+                            if (userInput.equals(terminateProgramKeyword)) {
+                                break;
+                            }
+                            String searchTerm = userInput;
+                            System.out.println("Enter search value");
+                            userInput = scanner.nextLine();
+                            if (userInput.equals(terminateProgramKeyword)) {
+                                break;
+                            }
+                            String searchValue = userInput;
+                            System.out.printf("Searching users for %s with a value of %s \n", searchTerm, searchValue);
+                            List<User> userList = Util.findUser(searchTerm, searchValue, usersFileName);
+                            if (userList.isEmpty()) {
+                                System.out.println("No results found");
+                            } else {
+                                userList.stream().forEach(user -> System.out.format("%s", user.toString()));
+                            }
+                        } else if(userInput.equals("2") && !userInput.equals(terminateProgramKeyword)) {
+                            System.out.println("in tickets search");
                         }
                     }
-                } else if (userInput.equals("2")) {
-                    System.out.println("in the tickets block");
-                }  else if (userInput.equals("")) {
-                    System.out.println("Enter key was pressed");
-                }  else {
+                    if (userInput.equals("2")) {
+                        System.out.println("View searchable fields");
+                    }
+                    if (userInput.equals("")) {
+                        //DO NOTHING
+                    }
+                } else if(userInput.equals(terminateProgramKeyword)) {
+                    break;
+                } else {
                     System.out.println("Invalid search option");
                     Util.printHelp(searchOptions);
                 }
-            }
 
+            }
         } catch (Exception e) {
             Util.printHelp(searchOptions);
             e.printStackTrace();
